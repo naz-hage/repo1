81a070a1853d29679d9c009f39be1a5be241e5ac
nhage <nhage@comcast.net>
2024-05-07 12:52:45 -0500

    01 issue (#2)
    The most significant changes in your workspace include updates to the `.gitignore` file, refactoring of the `github-release-notes.ps1` script, creation of a new module `github.psm1`, and refactoring of the `github-create-release.ps1` script. 
    
    1. `.gitignore` file was updated to ignore certain directories and files, preventing Git from tracking changes in these locations.
    2. `github-release-notes.ps1` script was refactored for improved flexibility and maintainability. Parameters are now passed as script parameters instead of being hard-coded. The script was restructured to include a `Main` function, and the release notes are now written to a file called `releasenotes.md`.
    3. A new module `github.psm1` was created, including functions `Get-LatestReleaseRaw` and `Get-LatestRelease` which fetch the latest release and all commits since the latest release from a GitHub repository.
    4. `github-create-release.ps1` script was refactored for improved modularity and reusability. Several functions were moved to the `github.psm1` module. A new function, `CreateAndPushTag`, was added to the `github.psm1` module, which is now used by the `Main` function in `github-create-release.ps1`.
    5. The `releasenotes.md` file was updated with the release notes for the latest changes.

--- github-create-release.ps1
+++ github-create-release.ps1
@@ -4,101 +4,9 @@ param (
     [string]$tag        
 )
 
-function Get-LatestRelease {
-    param (
-        [string]$token,
-        [string]$owner,
-        [string]$repo
-    )
+# Import the latest version of the module
+Import-Module -Name "./github.psm1" -Force
 
-# Get the latest release
-$headers = @{
-    "Authorization" = "token $token"
-    "Accept" = "application/vnd.github.v3+json"
-}
-
-try {
-    $uri = "https://api.github.com/repos/$owner/$repo/releases/latest"
-    Write-Host "Calling $uri"
-    $latestRelease = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
-        
-    return $latestRelease
-    }
-    catch {
-        Write-Host $_.Exception
-        Write-Host "Error: Could not get the latest release."
-        return $null
-    }
-}
-
-# Build release notes since a specific release
-function Get-CommitsSinceRelease {
-    param (
-        [string]$token,
-        [string]$owner,
-        [string]$repo,
-        [string]$sinceLastPublished
-    )
-
-    # Define headers
-    $headers = @{
-        "Authorization" = "token $token"
-        "Accept" = "application/vnd.github.v3+json"
-    }
-
-    # Get commits since the latest release
-    $commits = Invoke-RestMethod -Uri "https://api.github.com/repos/$owner/$repo/commits?since=$sinceLastPublished" -Method Get -Headers $headers
-
-    # Format the commit messages
-    $releaseNotes = "## What's Changed"
-    $releaseNotes += ($commits | ForEach-Object { "* $($_.commit.message) by @$_author in https://github.com/$owner/$repo/pull/$($_.sha)" }) -join "`n"
-    $releaseNotes += "`n`n## New Contributors"
-    $releaseNotes += ($commits | Where-Object { $_.author.login -notin $commits.author.login } | ForEach-Object { "* @$($_.author.login) made their first contribution in https://github.com/$owner/$repo/pull/$($_.sha)" }) -join "`n"
-    $releaseNotes += "`n`n**Full Changelog**: https://github.com/$owner/$repo/compare/$sinceLastPublished...$tag"
-
-    return $releaseNotes
-}
-
-function CreateGitHubRelease {
-    param (
-        [string]$token,
-        [string]$owner,
-        [string]$repo,
-        [string]$tag,
-        [string]$releaseName,
-        [string]$commitMessage,
-        [string]$assetPath,
-        [string]$assetName
-    )
-
-    $assetMimeType = "application/octet-stream"
-    $assetContent = [System.IO.File]::ReadAllBytes($assetPath)
-
-    $headers = @{
-        "Authorization" = "token $token"
-        "Accept" = "application/vnd.github.v3+json"
-    }
-
-    $body = @{
-        "tag_name" = $tag
-        "name" = $releaseName
-        "body" = $commitMessage
-        "draft" = $false
-        "prerelease" = $false
-    } | ConvertTo-Json
-
-    $response = Invoke-RestMethod -Uri "https://api.github.com/repos/$owner/$repo/releases" -Method Post -Body $body -Headers $headers
-
-    try {
-        $uploadUrl = $response.upload_url -replace "\{\?name,label\}", "?name=$assetName"
-        Write-Output "Upload URL: $uploadUrl"
-        Invoke-RestMethod -Uri $uploadUrl -Method Post -Body $assetContent -Headers $headers -ContentType $assetMimeType
-    } catch {
-        Write-Output $_.Exception
-        Write-Output "Error: Could not create a release."
-        exit 1
-    }
-}
 
 function Main {
     param (
@@ -106,29 +14,22 @@ function Main {
         [string]$tag        
     )
 
+    # Call the function to create and push the tag
+    CreateAndPushTag -tag $tag
+
     Write-Output "Creating release for tag: $tag"
     # Define parameters
     $token = Get-Content "C:\Users\Naz\token.env"
     $owner = "naz-hage"
-    $artifactPath = "c:/Artifacts/ntools/release"
+    $artifactPath = "c:/Artifacts/$repo/release"
     $releaseName = "$tag"
-    $commitMessage = "Test commit message for $tag"
+
     $assetPath = "$artifactPath/$tag.zip"
     $assetName = "$tag.zip"
 
-    # Get the latest release
-    $latestRelease = Get-LatestRelease -token $token -owner $owner -repo $repo
+    $releaseNotes = Get-LatestRelease -token $token -owner $owner -repo $repo -tag $tag
 
-    # Build release notes since the last published release
-    if ($null -eq $latestRelease) {
-        $sinceLastPublished = "1970-01-01T00:00:00Z"
-    } else {
-        $sinceLastPublished = $latestRelease.published_at
-    }
 
-    $releaseNotes = Get-CommitsSinceRelease -token $token -owner $owner -repo $repo -sinceLastPublished $sinceLastPublished
-
-    $commitMessage = $releaseNotes
     $assetPath = "$artifactPath/$tag.zip"
     $assetName = "$tag.zip"
 
@@ -139,7 +40,7 @@ function Main {
     Write-Output "Asset Name: $assetName"
 
     # Create Release
-    CreateGitHubRelease -token $token -owner $owner -repo $repo -tag $tag -releaseName $releaseName -commitMessage $commitMessage -assetPath $assetPath -assetName $assetName
+    CreateGitHubRelease -token $token -owner $owner -repo $repo -tag $tag -releaseName $releaseName -commitMessage $releaseNotes -assetPath $assetPath -assetName $assetName
 }
 
 # Call the main function with the provided tag
